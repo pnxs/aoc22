3fc88a8fac1bf1bf284d2e723baf94f3844ff813
Thomas Schätzlein <thomas@pnxs.de>
2022-12-06 08:03:41 +0100

    Day6: make it smaller

--- AoC22/AoC22/Day6/Day6.cs
+++ AoC22/AoC22/Day6/Day6.cs
@@ -4,31 +4,18 @@ public class Day6: IPuzzle
 {
     public void Solve()
     {
-        string path = "Day6/input.txt";
-        var streamReader = new StreamReader(path);
-        Console.WriteLine($"Part1: {FindMarker(streamReader, 4)}");
-
-        streamReader = new StreamReader(path);
-        Console.WriteLine($"Part2: {FindMarker(streamReader, 14)}");
+        var input = new StreamReader("Day6/input.txt").ReadToEnd();
+        Console.WriteLine($"Part1: {FindMarker(input, 4)}");
+        Console.WriteLine($"Part2: {FindMarker(input, 14)}");
     }
 
-    private static int FindMarker(TextReader reader, int distinctChars)
+    private static int FindMarker(string input, int distinctChars)
     {
-        var queue = new Queue<char>();
-
-        int pos = 0;
-        int ret;
-        while ((ret = reader.Read()) != -1)
+        for (var i = 0; i < input.Length; ++i)
         {
-            var ch = (char)ret;
-            ++pos;
-            queue.Enqueue(ch);
-            if (queue.Count <= distinctChars) continue;
-            queue.Dequeue();
-
-            if (queue.ToHashSet().Count == distinctChars)
+            if (input[i..(i+distinctChars)].Distinct().Count() == distinctChars)
             {
-                return pos;
+                return i + distinctChars;
             }
         }
         return -1;
